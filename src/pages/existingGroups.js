a5216d1b41e95faf9dda7e30fab7a43dcccc9625
WEIRDLEMON <utkarshdhy@gmail.com>
2018-12-30 18:02:49 +0530

    existing groups added

--- src/pages/existingGroups.js
+++ src/pages/existingGroups.js
@@ -11,17 +11,61 @@ var db =fire.firestore();
 
 class existingGroups extends Component{
 
+  constructor(props){
+    super(props)
+    this.state=({
+      arr:[]
+    })
+  }
   componentDidMount(){
          remoteActions.setListenerOnAuthChange()
     }
+
+  showGroups(){
+    console.log("hello")
+
+    db.collection("users").where("uid", "==", appStore.currentUser.uid)
+    .get()
+    .then((querySnapshot)=> {
+        querySnapshot.forEach((doc)=> {
+            // doc.data() is never undefined for query doc snapshots
+            var groupDict= doc.data().groups;
+            var arrGroup=[]
+            for(var x in groupDict){
+              arrGroup.push({
+                [x]: groupDict[x]
+              })
+            }
+            this.setState({
+              arr: arrGroup
+            })
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+  }
   render(){
     return(
       <Fragment>
         {
             appStore.auth.isLoggedIn ?
-              (
+              ( <Fragment>
                 <Header1/>
+                <button className='btn green' onClick={()=>{this.showGroups()}}>Click to show existing groups for this user</button>
+                <ul class="collection white">
+                {
+                  this.state.arr.map(function(m, i){
 
+                      for(var x in m){
+                        var y= m[x]
+                      }
+                      return(
+                      <li class="collection-item white"><p key={i}><Link to={"/groups" + x}>  {y}</Link> </p></li>
+          )
+        })
+      }
+                </ul></Fragment>
               )
             :
 
