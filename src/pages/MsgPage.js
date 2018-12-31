175a6613aa6aa256305cfa0a612b406e1541a063
WEIRDLEMON <utkarshdhy@gmail.com>
2018-12-31 13:53:30 +0530

    add participants

--- src/pages/MsgPage.js
+++ src/pages/MsgPage.js
@@ -5,20 +5,25 @@ import * as remoteActions from '../scripts/remoteActions.js';
 import { observer } from 'mobx-react';
 import Header1 from '../components/Header1';
 import fire from '../scripts/fire.js';
-
+import firebase from 'firebase';
 var db =fire.firestore();
+
 class MsgPage extends Component{
   constructor(props){
     super(props)
     this.state={
       groupId:null,
-      groupName: null
+      groupName: null,
+      messages:[],
+      newMessage:''
     }
   }
 
   grouping(){
     var groupId= this.props.match.params.groupId;
     console.log(groupId);
+
+    //IDHAR MID KA PRAYOG HUA
     db.collection("groups").where("gid", "==", groupId)
     .get()
     .then((querySnapshot)=> {
@@ -26,7 +31,8 @@ class MsgPage extends Component{
             // doc.data() is never undefined for query doc snapshots
             this.setState({
               groupId: doc.data().gid,
-              groupName: doc.data().name
+              groupName: doc.data().name,
+              type:""
             })
         });
     })
@@ -34,8 +40,70 @@ class MsgPage extends Component{
         console.log("Error getting documents: ", error);
     });
   }
-componentDidMount(){
-  remoteActions.setListenerOnAuthChange();
+  componentDidUpdate() {
+    console.log("Component Did Update!");
+  }
+
+  componentDidMount(){
+    remoteActions.setListenerOnAuthChange();
+  }
+
+handleClick(){
+  console.log("hooo")
+  var gid= this.props.match.params.groupId;
+  var mid= firebase.firestore.FieldValue.serverTimestamp();
+  var msgDict={
+    body : this.state.newMessage,
+    mid,
+    gid,
+    uid: appStore.currentUser.uid
+  }
+
+//THE adding message GROUP SHIT
+db.collection("messages").add({
+  body: msgDict.body,
+  mid: msgDict.mid,
+  gid: msgDict.gid,
+  uid: msgDict.uid
+})
+.then(function(docRef) {
+    console.log("messages written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding messages ", error);
+});
+
+//THE retrieving message GROUP SHIT
+  db.collection("messages").where("gid", "==", gid).orderBy("mid")
+  .get()
+  .then((querySnapshot)=> {
+      var arr = []
+
+      querySnapshot.forEach((doc)=> {
+          // doc.data() is never undefined for query doc snapshots
+          var msggD= doc.data()
+          arr.push(msggD)
+      });
+
+      this.setState({
+        messages: arr
+      });
+
+      console.log(this.state.messages)
+  })
+  .catch(function(error) {
+      console.log("Error getting documents: ", error);
+  });
+
+
+
+
+
+
+
+
+
+
 }
 
 render(){
@@ -50,6 +118,34 @@ render(){
               }
                 <Header1/>
                 <div class="col s12 white"><h1>{this.state.groupName}</h1></div>
+
+
+                <div class="row">
+                  <form class="col s12">
+                    <div class="row">
+                      <div class="input-field col s12">
+                        <input value={this.state.newMessage} id="email"  class="active" onChange={(e)=>{
+                          this.setState({
+                                newMessage:e.target.value
+                              })
+                        }}/>
+
+                        <span class="helper-text " data-error="wrong" data-success="right"><font color="green">Write a message</font></span>
+                      </div>
+                    </div>
+                  </form>
+
+                  <button class="btn waves-effect waves-light center-align" type="submit" name="action" onClick={()=>{this.handleClick()}}>Submit
+                  <i class="material-icons right">send</i>
+                </button>
+                {
+                  this.state.messages.map((m, i)=>{
+                    return(
+                      <div class="white"><pre key={i}><b>{m.body}</b> by<font color="green"> {m.uid} <b>AT</b> </font></pre></div>
+                    )
+                  })
+                }
+                </div>
               </Fragment>
             )
              :
