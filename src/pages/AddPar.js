e19055072b634ceaea7352075f4d59434d819320
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-01 02:22:24 +0530

    installed and removed several packages

--- src/pages/AddPar.js
+++ src/pages/AddPar.js
@@ -9,7 +9,26 @@ import firebase from 'firebase';
 
 
 class AddPar extends Component{
-
+// kootah(){
+//   function listAllUsers(nextPageToken) {
+//   // List batch of users, 1000 at a time.
+//   admin.auth().listUsers(1000, nextPageToken)
+//     .then(function(listUsersResult) {
+//       listUsersResult.users.forEach(function(userRecord) {
+//         console.log("user", userRecord.toJSON());
+//       });
+//       if (listUsersResult.pageToken) {
+//         // List next batch of users.
+//         listAllUsers(listUsersResult.pageToken)
+//       }
+//     })
+//     .catch(function(error) {
+//       console.log("Error listing users:", error);
+//     });
+// }
+// // Start listing users from the beginning, 1000 at a time.
+// listAllUsers();
+// }
 render(){
   return(
 
@@ -20,6 +39,9 @@ render(){
                 (
                   <Fragment>
                     <Header1/>
+                    {
+
+                    }
                   </Fragment>
 
                 )
