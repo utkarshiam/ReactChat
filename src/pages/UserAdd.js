477f4c611ced90ccd6f077c50292a4df82c910be
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-03 02:05:44 +0530

    peeps messaging ke naam

--- src/pages/UserAdd.js
+++ src/pages/UserAdd.js
@@ -16,7 +16,8 @@ class UserAdd extends Component{
       //add shit
       uid:null,
       name:null,
-      groupId:null
+      groupId:null,
+      groupName:null
     })
   }
 
@@ -43,21 +44,48 @@ class UserAdd extends Component{
     });
   }
 kootah(){
-  // db.collection("groups").where("uid", "==", uid)
-  // .get()
-  // .then((querySnapshot)=> {
-  //     querySnapshot.forEach((doc)=> {
-  //         // doc.data() is never undefined for query doc snapshots
-  //         this.setState({
-  //           uid: doc.data().uid,
-  //           name: doc.data().name,
-  //
-  //         })
-  //     });
-  // })
-  // .catch(function(error) {
-  //     console.log("Error getting documents: ", error);
-  // });
+  var groupId= this.props.match.params.groupId;
+  console.log(groupId);
+  var userDict={
+    [this.state.uid]:this.state.name
+  }
+
+  var groupDict={}
+
+  db.collection("groups").where("gid", "==", groupId)
+  .get()
+  .then((querySnapshot)=> {
+      querySnapshot.forEach((doc)=> {
+          // doc.data() is never undefined for query doc snapshots
+          var docId= doc.id;
+          this.setState({
+            groupId: groupId,
+            groupName: doc.data().name
+          })
+          groupDict={
+            [this.state.groupId]:this.state.groupName
+          }
+          db.collection("groups").doc(docId).set({
+            users: userDict
+          },{merge:true})
+          console.log("success")
+          })
+      });
+
+      db.collection("users").where("uid", "==",this.state.uid)
+      .get()
+      .then((querySnapshot)=> {
+          querySnapshot.forEach((doc)=> {
+              // doc.data() is never undefined for query doc snapshots
+              var docId= doc.id;
+
+              db.collection("users").doc(docId).set({
+                groups: groupDict
+              },{merge:true})
+              console.log("success")
+              })
+          });
+
 
 
 }
